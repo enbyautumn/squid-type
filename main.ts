30cf3807ae0fc87c3b2b9486e94851a10fd1ed1f
alexdcdc <alexdcdc@gmail.com>
2022-03-10 11:14:28 -0500

    No

--- main.ts
+++ main.ts
@@ -1,31 +1,55 @@
 console.log("aeiou")
 let typer = document.getElementById("typer")
 let text = typer.innerText
-
-function generateSpan(name, start, end) {
-
-}
+//let validLetters = new RegExp("[a-zA-Z0-9()\-:;.,?!\"']")
+// let validLetters = RegExp("[a-zA-Z0-9]")
+let validLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.".split('')
 
 console.log(text)
 
 
 let currentPos = 0
-/*
-let untypedStart = 0
-let untypedEnd = 0
-let correctStart = 0
-let correctEnd = 0
 let incorrectStart = 0
-let incorrectEnd = 0
-*/
+let incorrect = false;
 
+
+function clamp(number, min, max) {
+    if (number < min) {
+        number = min
+    }
+    else if (number > max) {
+        number = max
+    }
+
+    return number
+}
+
+function filter(c) {
+    let validLetters = RegExp("/[a-zA-Z0-9()\-:;.,?!\"']/")
+    if (validLetters.test(c)) {
+        console.log("based")
+    }
+}
 
 document.addEventListener("keydown", e => {
-    currentPos++
+    console.log(e.key)
+    console.log(validLetters.test(e.key))
+    if (e.key == "Backspace") {
+        currentPos--
+    }
+    else {
+        
+        if (e.key == text[currentPos]) {
+            currentPos++
+        }
+        
+    }
+        currentPos = clamp(currentPos, 0, text.length)
+
     let typedText = text.slice(0, currentPos)
     let untypedText = text.slice(currentPos, text.length)
 
-    let formattedTyped = `<span class = "typed">${typedText}</span>`
+    let formattedTyped = `<span class = "correct">${typedText}</span>`
     let formattedUntyped = `<span class = "untyped">${untypedText}</span>`
 
     typer.innerHTML = formattedTyped + formattedUntyped;
--- out.js
+++ out.js
@@ -3,13 +3,31 @@
   console.log("aeiou");
   var typer = document.getElementById("typer");
   var text = typer.innerText;
+  var validLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.".split("");
   console.log(text);
   var currentPos = 0;
+  function clamp(number, min, max) {
+    if (number < min) {
+      number = min;
+    } else if (number > max) {
+      number = max;
+    }
+    return number;
+  }
   document.addEventListener("keydown", (e) => {
-    currentPos++;
+    console.log(e.key);
+    console.log(validLetters.test(e.key));
+    if (e.key == "Backspace") {
+      currentPos--;
+    } else {
+      if (e.key == text[currentPos]) {
+        currentPos++;
+      }
+    }
+    currentPos = clamp(currentPos, 0, text.length);
     let typedText = text.slice(0, currentPos);
     let untypedText = text.slice(currentPos, text.length);
-    let formattedTyped = `<span class = "typed">${typedText}</span>`;
+    let formattedTyped = `<span class = "correct">${typedText}</span>`;
     let formattedUntyped = `<span class = "untyped">${untypedText}</span>`;
     typer.innerHTML = formattedTyped + formattedUntyped;
   });
