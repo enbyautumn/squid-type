e3a217cb71ed350718183ab9125de8dc193ae7c5
alexdcdc <alexdcdc@gmail.com>
2022-03-10 15:08:39 -0500

    added working error check

--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
 console.log("aeiou")
 let typer = document.getElementById("typer")
 let text = typer.innerText
-let validLetters = new RegExp(/[a-zA-Z0-9()\-:;.,?!"']/m)
+text = text.replace(/[^a-zA-Z0-9()\-:;.,?!"' ]/g, "")
+typer.innerHTML = `<span class = "untyped">${text}</span>`
 
+let validLetters = new RegExp(/[a-zA-Z0-9()\-:;.,?!"' ]/m)
 
-console.log(text)
+//regular expression including all valid characters you can type in the passage
 
 
 let currentPos = 0
 let incorrectStart = 0
-let incorrect = false;
+let incorrect = false
 
 
-function clamp(number, min, max) {
+function clamp(number, min, max) {//clamps a number between two bounds
     if (number < min) {
         number = min
     }
@@ -25,28 +27,46 @@ function clamp(number, min, max) {
 
 
 document.addEventListener("keydown", e => {
-    if (e.key == "Backspace") {
+
+    if (e.key == "Backspace") { //on backspace, move the cursor back
         currentPos--
+        if (currentPos <= incorrectStart) {
+            incorrect = false
+            //if cursor is behind the incorrect portion, no more incorrect text
+        }
     }
+
     else {
-        if (e.key.length == 1 && validLetters.test(e.key)) {
-            console.log(e.key)
-            if (e.key != text[currentPos]) {
-                incorrect = true
+        if (e.key.length == 1 && validLetters.test(e.key)) { 
+            //checks that key pressed is a valid character
+            if (e.key != text[currentPos] && !incorrect) {
+                //checks beginning of mistake
                 incorrectStart = currentPos
+                incorrect = true
             }
-            currentPos++;
+
+            currentPos++
+            //if valid key typed, move cursor regardless of correctness
         }
         
     }
+
     currentPos = clamp(currentPos, 0, text.length)
 
-    let typedText = text.slice(0, currentPos)
-    let untypedText = text.slice(currentPos, text.length)
+    if (!incorrect) {
+        incorrectStart = currentPos
+    } //saves start of mistake if there is a mistake, does not otherwise
 
-    let formattedTyped = `<span class = "correct">${typedText}</span>`
+
+    let correctText = text.slice(0, incorrectStart)
+    let incorrectText = text.slice(incorrectStart, currentPos)
+    let untypedText = text.slice(currentPos, text.length)
+    //gets three chunks: correct text, incorrect text, untyped text
+
+    let formattedCorrect = `<span class = "correct">${correctText}</span>`
+    let formattedIncorrect = `<span class = "incorrect">${incorrectText}</span>`
     let formattedUntyped = `<span class = "untyped">${untypedText}</span>`
 
-    typer.innerHTML = formattedTyped + formattedUntyped;
-    
+    typer.innerHTML = formattedCorrect + formattedIncorrect + formattedUntyped
+    //formats text according to type and joins it in order
 })
--- out.js
+++ out.js
@@ -3,8 +3,9 @@
   console.log("aeiou");
   var typer = document.getElementById("typer");
   var text = typer.innerText;
-  var validLetters = new RegExp(/[a-zA-Z0-9()\-:;.,?!"']/m);
-  console.log(text);
+  text = text.replace(/[^a-zA-Z0-9()\-:;.,?!"' ]/g, "");
+  typer.innerHTML = `<span class = "untyped">${text}</span>`;
+  var validLetters = new RegExp(/[a-zA-Z0-9()\-:;.,?!"' ]/m);
   var currentPos = 0;
   var incorrectStart = 0;
   var incorrect = false;
@@ -19,21 +20,28 @@
   document.addEventListener("keydown", (e) => {
     if (e.key == "Backspace") {
       currentPos--;
+      if (currentPos <= incorrectStart) {
+        incorrect = false;
+      }
     } else {
       if (e.key.length == 1 && validLetters.test(e.key)) {
-        console.log(e.key);
-        if (e.key != text[currentPos]) {
-          incorrect = true;
+        if (e.key != text[currentPos] && !incorrect) {
           incorrectStart = currentPos;
+          incorrect = true;
         }
         currentPos++;
       }
     }
     currentPos = clamp(currentPos, 0, text.length);
-    let typedText = text.slice(0, currentPos);
+    if (!incorrect) {
+      incorrectStart = currentPos;
+    }
+    let correctText = text.slice(0, incorrectStart);
+    let incorrectText = text.slice(incorrectStart, currentPos);
     let untypedText = text.slice(currentPos, text.length);
-    let formattedTyped = `<span class = "correct">${typedText}</span>`;
+    let formattedCorrect = `<span class = "correct">${correctText}</span>`;
+    let formattedIncorrect = `<span class = "incorrect">${incorrectText}</span>`;
     let formattedUntyped = `<span class = "untyped">${untypedText}</span>`;
-    typer.innerHTML = formattedTyped + formattedUntyped;
+    typer.innerHTML = formattedCorrect + formattedIncorrect + formattedUntyped;
   });
 })();
